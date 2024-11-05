6caa955517b4504802a1967bc7e74e88cef62d8c
Artem Polyakov <nuk1xm0@gmail.com>
2024-11-06 00:59:12 +0300

    Create main menu

---
+++ MrNovgorod/Assets/Scripts/MainMenu/Installers/UIMainMenuInstaller.cs
@@ -0,0 +1,25 @@
+﻿using MainMenu.Presenters;
+using MainMenu.Views;
+using UnityEngine;
+using Zenject;
+
+namespace MainMenu.Installers
+{
+    public class UIMainMenuInstaller : MonoInstaller
+    {
+        [SerializeField] private UIMainMenuView _uiMainMenuView;
+        private UIMainMenuPresenter _presenter;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<UIMainMenuView>().FromInstance(_uiMainMenuView).AsSingle();
+            Container.Bind<UIMainMenuPresenter>().AsSingle().WithArguments(_uiMainMenuView);
+        }
+
+        public override void Start()
+        {
+            _presenter = Container.Resolve<UIMainMenuPresenter>();
+            _presenter.Initialize();
+        }
+    }
+}
---
+++ MrNovgorod/Assets/Scripts/SceneLoader/SceneLoaderInstaller.cs
@@ -0,0 +1,9 @@
+﻿using Zenject;
+
+public class SceneLoaderInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.BindInterfacesAndSelfTo<SceneLoader>().AsSingle();
+    }
+}
