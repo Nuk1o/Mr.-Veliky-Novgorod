2d8a096069897d79b6b4a59a94cad2fcf28f0fbe
Artem Polyakov <nuk1xm0@gmail.com>
2024-11-16 22:32:12 +0300

    Added new CameraControllers

---
+++ MrNovgorod/Assets/Scripts/Game/Camera Controller/CameraMoveController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Cinemachine;
+using UniRx;
+using UnityEngine;
+
+namespace GameCore
+{
+    public class CameraMoveController : MonoBehaviour, IDisposable
+    {
+        [SerializeField] private CinemachineVirtualCamera _camera;
+        [SerializeField] private float _cameraSpeed = 10f;
+        [SerializeField] private float _smoothTime = 0.1f;
+        
+        [Space]
+        [Header("Map restriction")]
+        [SerializeField] private float _minX = -10f;
+        [SerializeField] private float _maxX = 10f;
+        [SerializeField] private float _minZ = -10f;
+        [SerializeField] private float _maxZ = 10f;
+
+        private CompositeDisposable _disposables;
+        private Vector3 _targetPosition;
+        private Vector3 _velocity = Vector3.zero;
+
+        private void Start()
+        {
+            _disposables = new CompositeDisposable();
+            _targetPosition = _camera.transform.position;
+            Observable
+                .EveryFixedUpdate()
+                .Subscribe(_ => CameraMovementControls())
+                .AddTo(_disposables);
+        }
+
+        private void CameraMovementControls()
+        {
+            if (Input.touchCount <= 0 && !Input.GetMouseButton(0)) return;
+            Vector2 touchDelta;
+
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    touchDelta = touch.deltaPosition;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                touchDelta =
+                    new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * 100;
+            }
+            
+            float deltaX = -touchDelta.x * _cameraSpeed * Time.fixedDeltaTime;
+            float deltaZ = -touchDelta.y * _cameraSpeed * Time.fixedDeltaTime;
+            
+            _targetPosition.x += deltaX;
+            _targetPosition.z += deltaZ;
+            
+            _targetPosition.x = Mathf.Clamp(_targetPosition.x, _minX, _maxX);
+            _targetPosition.z = Mathf.Clamp(_targetPosition.z, _minZ, _maxZ);
+            
+            _camera.transform.position = Vector3.SmoothDamp(_camera.transform.position, _targetPosition, ref _velocity, _smoothTime);
+        }
+
+        public void Dispose()
+        {
+            _disposables.Dispose();
+        }
+    }
+}
---
+++ MrNovgorod/Assets/Scripts/Game/Camera Controller/CameraZoomController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Cinemachine;
+using UniRx;
+using UnityEngine;
+
+namespace GameCore
+{
+    public class CameraZoomController : MonoBehaviour, IDisposable
+    {
+        [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+        [SerializeField] private float _zoomSpeed = 0.5f;
+        [SerializeField] private float _minZoom = 2f;
+        [SerializeField] private float _maxZoom = 10f;
+        [SerializeField] private float _smoothTime = 0.1f;
+
+        private CompositeDisposable _disposable;
+        private float _currentZoom;
+        private float _targetZoom;
+
+        private void Start()
+        {
+            _currentZoom = _virtualCamera.m_Lens.FieldOfView;
+            _targetZoom = _currentZoom;
+            _disposable = new CompositeDisposable();
+
+            Observable
+                .EveryFixedUpdate()
+                .Subscribe(_ => ZoomCamera())
+                .AddTo(_disposable);
+        }
+
+        private void ZoomCamera()
+        {
+            if (Input.touchCount == 2)
+            {
+                Touch touchZero = Input.GetTouch(0);
+                Touch touchOne = Input.GetTouch(1);
+
+                float currentTouchDistance = Vector2.Distance(touchZero.position, touchOne.position);
+                float previousTouchDistance = Vector2.Distance(touchZero.position - touchZero.deltaPosition,
+                    touchOne.position - touchOne.deltaPosition);
+
+                float deltaDistance = currentTouchDistance - previousTouchDistance;
+
+                _targetZoom -= deltaDistance * _zoomSpeed;
+                _targetZoom = Mathf.Clamp(_targetZoom, _minZoom, _maxZoom);
+            }
+            
+            if (Input.mouseScrollDelta.y != 0)
+            {
+                _targetZoom -= Input.mouseScrollDelta.y * _zoomSpeed;
+                _targetZoom = Mathf.Clamp(_targetZoom, _minZoom, _maxZoom);
+            }
+            
+            _currentZoom = Mathf.Lerp(_currentZoom, _targetZoom, _smoothTime);
+            _virtualCamera.m_Lens.FieldOfView = _currentZoom;
+        }
+
+        public void Dispose()
+        {
+            _disposable?.Dispose();
+        }
+    }
+}
