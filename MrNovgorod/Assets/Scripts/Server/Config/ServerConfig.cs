c59f11e846d5c778665086156d076345294fed65
Artem Polyakov <nuk1xm0@gmail.com>
2024-11-17 04:09:29 +0300

    Save

--- MrNovgorod/Assets/Scripts/Server/Config/ServerConfig.cs
+++ MrNovgorod/Assets/Scripts/Server/Config/ServerConfig.cs
@@ -2,11 +2,7 @@
 {
     public class ServerConfig
     {
-        public const string SERVER_NAME = "Server";
         public const string SERVER_ADRESS = "http://vadimkt2.beget.tech";
-        public const int SERVER_PORT = 8080;
-        public const string SERVER_PASSWORD = "Password";
-        public const string SERVER_USERNAME = "UserName";
         public const string API_KEY = "2|7f1kineQ8yrIKMC0ZKZKOISZfKnpiuqWPRYV0u9oa52d9ca6";
     }
 }
