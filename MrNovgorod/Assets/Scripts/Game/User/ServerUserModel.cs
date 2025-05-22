4212d61f73e172dc6f1c508361e806e23b7eeef2
Artem Polyakov <nuki.xm@yandex.ru>
2025-05-23 01:19:33 +0300

    Save model

--- MrNovgorod/Assets/Scripts/Game/User/ServerUserModel.cs
+++ MrNovgorod/Assets/Scripts/Game/User/ServerUserModel.cs
@@ -1,7 +1,31 @@
 ﻿namespace Game.User
 {
+    [System.Serializable]
     public class ServerUserModel
     {
-        
+        public int id;
+        public string name;
+        public string email;
+        public bool email_verified_at;
+        public string avatar;
+        public string phone;
+        public ServerReview[] reviews; 
+    }
+    
+    [System.Serializable]
+    public class ServerReview
+    {
+        public int id;
+        public string comment;
+        public int rating;
+        public ServerAttraction attraction;
+    }
+    
+    [System.Serializable]
+    public class ServerAttraction
+    {
+        public int id;
+        public string name;
+        public string image;
     }
 }
